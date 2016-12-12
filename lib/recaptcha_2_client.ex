ec50208fbadba073f4dc91aa3aeefb3b0e77fff5
Ricardo Perez <ricardop@square-enix.com>
2016-12-12 16:58:21 +0000

    Make only verify public visible

--- lib/recaptcha_2_client.ex
+++ lib/recaptcha_2_client.ex
@@ -12,23 +12,23 @@ defmodule Recaptcha2Client do
     verify_http_status(http_response)
   end
 
-  def verify_http_status(http_response = %{status: 200}) do
+  defp verify_http_status(http_response = %{status: 200}) do
     verify_captcha_response(Poison.decode!(http_response.body))
   end
 
-  def verify_http_status(http_response = %{status: _}) do
+  defp verify_http_status(http_response = %{status: _}) do
     {:error, "problem connecting with recaptcha"}
   end
 
-  def verify_captcha_response(json_body = %{"success" => true}) do
+  defp verify_captcha_response(json_body = %{"success" => true}) do
     {:ok, true}
   end
 
-  def verify_captcha_response(json_body = %{"success" => false, "error-codes" => ["invalid-input-response"] }) do
+  defp verify_captcha_response(json_body = %{"success" => false, "error-codes" => ["invalid-input-response"] }) do
     {:error, "captcha is wrong"}
   end
 
-  def verify_captcha_response(json_body = %{"success" => false}) do
+  defp verify_captcha_response(json_body = %{"success" => false}) do
     {:error, "captcha timed out"}
   end
 end

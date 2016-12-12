8d3353676add4bf3ed166e8cbb396ca653bdd923
Ricardo Perez <ricardop@square-enix.com>
2016-12-12 17:28:21 +0000

    Remove warnings

--- lib/recaptcha_2_client.ex
+++ lib/recaptcha_2_client.ex
@@ -27,19 +27,19 @@ defmodule Recaptcha2Client do
     verify_captcha_response(Poison.decode!(http_response.body))
   end
 
-  defp verify_http_status(http_response = %{status: _}) do
+  defp verify_http_status(%{status: _}) do
     {:error, "problem connecting with recaptcha"}
   end
 
-  defp verify_captcha_response(json_body = %{"success" => true}) do
+  defp verify_captcha_response(%{"success" => true}) do
     {:ok, true}
   end
 
-  defp verify_captcha_response(json_body = %{"success" => false, "error-codes" => ["invalid-input-response"] }) do
+  defp verify_captcha_response(%{"success" => false, "error-codes" => ["invalid-input-response"] }) do
     {:error, "captcha is wrong"}
   end
 
-  defp verify_captcha_response(json_body = %{"success" => false}) do
+  defp verify_captcha_response(%{"success" => false}) do
     {:error, "captcha timed out"}
   end
 end
